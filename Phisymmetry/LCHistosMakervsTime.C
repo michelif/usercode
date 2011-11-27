b5f8475b9e9e121b7938e0cbc524a63770c983f9
micheli <>
2011-11-27 18:32:28 +0000

    optimization of time periods added

--- Phisymmetry/LCHistosMakervsTime.C
+++ Phisymmetry/LCHistosMakervsTime.C
@@ -28,17 +28,22 @@
 #define NJOBS_MIN 1
 #define NJOBS_MAX 34
 #define NJOBS NJOBS_MAX-NJOBS_MIN+1
-#define NROWSJOB 61200
-#define kBarlRings 85
+#define kBarlRings 85//change!
 #define kBarlWedges 360
 #define kSides 2
+#define NROWSJOB kBarlRings*kBarlWedges*kSides
 
 using namespace std;
 
 void LCHistoMakerBarrelvsTime(){
-  cout<<"start"<<endl; 
-  TFile f("LCHistosvsTime.root","recreate");
+  // cout<<"start"<<endl; 
 
+  
+  stringstream nameFileStream;
+  nameFileStream<<"LCHistosvsTime_"<<NJOBS_MIN<<"_"<<NJOBS_MAX<<".root";
+
+
+  TFile f(nameFileStream.str().c_str(),"recreate");
 
   int counter=0,counterJob=0;
   int ieta,iphi,sign,nhits;
@@ -46,12 +51,12 @@ void LCHistoMakerBarrelvsTime(){
  
   float LCmap[kBarlRings][kBarlWedges][kSides];
   int nhitsMap[kBarlRings][kBarlWedges][kSides];
- float LCvsTime[kBarlRings][kBarlWedges][kSides][NJOBS];
+  float LCvsTime[kBarlRings][kBarlWedges][kSides][NJOBS];
   TGraph* graphLCvsTime[kBarlRings][kBarlWedges][kSides];
 
   float avgTime[NJOBS];
 
-  cout<<"start"<<endl;
+  //  cout<<"start"<<endl;
   //initializing variables
     for(int iieta=0;iieta<kBarlRings;iieta++){
       for(int iiphi=0;iiphi<kBarlWedges;iiphi++){
@@ -59,8 +64,8 @@ void LCHistoMakerBarrelvsTime(){
 	  LCmap[iieta][iiphi][iisign]=0.;
 	  nhitsMap[iieta][iiphi][iisign]=0;
 	  for(int iJob=0;iJob<NJOBS;iJob++){
-    LCvsTime[iieta][iiphi][iisign][iJob]=0.;
-	    cout<<"iieta "<<iieta<<" iiphi "<<iiphi<< " iisign "<<iisign<<" iJob "<<iJob<<endl; 
+	    LCvsTime[iieta][iiphi][iisign][iJob]=0.;
+	    //	    cout<<"iieta "<<iieta<<" iiphi "<<iiphi<< " iisign "<<iisign<<" iJob "<<iJob<<endl; 
 	  }
 	}
       }
@@ -79,8 +84,8 @@ void LCHistoMakerBarrelvsTime(){
     exit(0);
   }
 
-  while(!LC_barl_in.eof()){
-    cout<<"entering"<<endl;
+  while(!LC_barl_in.eof() && counterJob< NJOBS){
+    //    cout<<"entering"<<endl;
     int counterRow=0;
 
     while(counterRow<NROWSJOB){
@@ -102,6 +107,7 @@ void LCHistoMakerBarrelvsTime(){
 	for(int iisign=0;iisign<kSides;iisign++){
 	  	  if(nhitsMap[iieta][iiphi][iisign] !=0) LCvsTime[iieta][iiphi][iisign][counterJob]= (float)LCmap[iieta][iiphi][iisign]/nhitsMap[iieta][iiphi][iisign];
 
+		  //cout<<LCvsTime[iieta][iiphi][iisign][counterJob]<<endl;
 	  LCmap[iieta][iiphi][iisign]=0.;  
 	  nhitsMap[iieta][iiphi][iisign]=0;  
 	}
@@ -112,18 +118,29 @@ void LCHistoMakerBarrelvsTime(){
     counterJob++;
 
   }
+
+
+  f.cd();
   for(int iieta=0;iieta<kBarlRings;iieta++){
+    cout<<"iieta "<<iieta<<endl;
     for(int iiphi=0;iiphi<kBarlWedges;iiphi++){
       for(int iisign=0;iisign<kSides;iisign++){
-	graphLCvsTime[iieta][iiphi][iisign]= new TGraph(NJOBS,LCvsTime[iieta][iiphi][iisign],avgTime);  
+
+	graphLCvsTime[iieta][iiphi][iisign]= new TGraph(NJOBS,avgTime,LCvsTime[iieta][iiphi][iisign]);  
 	string dummyString;
 	stringstream dummyStream;
-	dummyStream<<"LCvsEta_"<<iieta<<"_"<<iphi<<"_"<<iisign;
+	dummyStream<<"LCvsEta_"<<iieta<<"_"<<iiphi<<"_"<<iisign;
 	dummyString=dummyStream.str();
+	graphLCvsTime[iieta][iiphi][iisign]->GetYaxis()->SetTitle("LC");
+	graphLCvsTime[iieta][iiphi][iisign]->GetXaxis()->SetTitle("unixtime");
 	graphLCvsTime[iieta][iiphi][iisign]->SetName(dummyString.c_str());
+	graphLCvsTime[iieta][iiphi][iisign]->SetTitle(dummyString.c_str());
+	graphLCvsTime[iieta][iiphi][iisign]->Write();
       }
     }
   }
 
+  f.Write();
+  f.Close();
 
 }
