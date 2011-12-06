9755fd14dac12baab4a576c5186ad9b1d0c11564
micheli <>
2011-12-06 15:35:25 +0000

    program to creat chain and execute plotsVsTimeMacro_barl.C

---
+++ Phisymmetry/treePrograms/createClassMacroVsTime_barl.C
@@ -0,0 +1,34 @@
+{
+
+TChain inputChain_barl("variablesTree_barl");
+TChain inputChain_endc("variablesTree_endc");
+
+std::ifstream fileList("filelist.txt", ios::in);
+
+  if (!fileList.is_open()) {
+    cout<<"file not found"<<endl;
+    exit(0);
+  }
+  
+
+
+int sum=0;
+ while(!fileList.eof() ){
+  string nameFile;
+  getline(fileList,nameFile);
+  inputChain_barl.Add(nameFile.c_str());
+  inputChain_endc.Add(nameFile.c_str());
+  sum++;
+  cout<<sum<<endl;
+ }
+
+
+
+
+
+
+ gROOT->ProcessLine(".L plotsVsTimeMacro_barl.C+");
+ gROOT->ProcessLine("plotsVsTimeMacro_barl t(&inputChain_barl)");
+ gROOT->ProcessLine("t.Loop()");
+
+}
